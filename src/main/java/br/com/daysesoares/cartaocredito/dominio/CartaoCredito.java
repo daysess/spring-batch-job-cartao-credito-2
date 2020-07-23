3e2a68906122646371b79618f7f8a5b52187d662
Dayse Soares <daysedesign@gmail.com>
2020-07-23 20:40:56 -0300

    Criacao dos dominios

---
+++ src/main/java/br/com/daysesoares/cartaocredito/dominio/CartaoCredito.java
@@ -0,0 +1,34 @@
+package br.com.daysesoares.cartaocredito.dominio;
+
+public class CartaoCredito {
+
+	private int numeroCartaoCredito;
+	private Cliente cliente; 
+	
+	public CartaoCredito() {
+		
+	}
+
+	public CartaoCredito(int numeroCartaoCredito, Cliente cliente) {
+		super();
+		this.numeroCartaoCredito = numeroCartaoCredito;
+		this.cliente = cliente;
+	}
+
+	public int getNumeroCartaoCredito() {
+		return numeroCartaoCredito;
+	}
+
+	public void setNumeroCartaoCredito(int numeroCartaoCredito) {
+		this.numeroCartaoCredito = numeroCartaoCredito;
+	}
+
+	public Cliente getCliente() {
+		return cliente;
+	}
+
+	public void setCliente(Cliente cliente) {
+		this.cliente = cliente;
+	}
+	
+}
---
+++ src/main/java/br/com/daysesoares/cartaocredito/dominio/FaturaCartaoCredito.java
@@ -0,0 +1,50 @@
+package br.com.daysesoares.cartaocredito.dominio;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FaturaCartaoCredito {
+
+	private Cliente cliente;
+	private CartaoCredito cartaoCredito;
+	private List<Transacao> transacoes = new ArrayList<>();
+	
+	public FaturaCartaoCredito() {
+		
+	}
+
+	public FaturaCartaoCredito(Cliente cliente, CartaoCredito cartaoCredito, List<Transacao> transacoes) {
+		super();
+		this.cliente = cliente;
+		this.cartaoCredito = cartaoCredito;
+		this.transacoes = transacoes;
+	}
+
+	public Cliente getCliente() {
+		return cliente;
+	}
+
+	public void setCliente(Cliente cliente) {
+		this.cliente = cliente;
+	}
+
+	public CartaoCredito getCartaoCredito() {
+		return cartaoCredito;
+	}
+
+	public void setCartaoCredito(CartaoCredito cartaoCredito) {
+		this.cartaoCredito = cartaoCredito;
+	}
+
+	public List<Transacao> getTransacoes() {
+		return transacoes;
+	}
+
+	public Double getTotal() {
+		return transacoes
+				.stream()
+				.mapToDouble(Transacao::getValor)
+				.reduce(0.0, Double::sum);
+	}
+	
+}
