f9fc4a9644877311d3747ff5ada0ee23a3232321
Dayse Soares <daysedesign@gmail.com>
2020-07-23 20:24:16 -0300

    Projeto inicial

---
+++ src/main/java/br/com/daysesoares/cartaocredito/SpringBatchJobCartaoCredito2Application.java
@@ -0,0 +1,13 @@
+package br.com.daysesoares.cartaocredito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBatchJobCartaoCredito2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBatchJobCartaoCredito2Application.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/daysesoares/cartaocredito/SpringBatchJobCartaoCredito2ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.daysesoares.cartaocredito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBatchJobCartaoCredito2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
